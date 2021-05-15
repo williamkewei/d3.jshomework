96c0a633d420dff081d043438c8d7bc28aa8721e
williamkewei <77704611+williamkewei@users.noreply.github.com>
2021-05-15 14:03:05 +1000

    home

--- plot.js
+++ plot.js
@@ -1,82 +1,42 @@
-// @TODO: Complete the Following Steps
+// Use d3.json() to fetch data from JSON file
+// Incoming data is internally referred to as incomingData
+d3.json("data/data.json").then((incomingData) => {
+  function filterMovieRatings(movie) {
+    return movie.imdbRating > 8.9;
+  }
 
-// Filter the top 15 cities with a population increase greater than 15,000
-//  and then graph each city on the x-axis and the respective population on the y-axis.
+  // Use filter() to pass the function as its argument
+  var filteredMovies = incomingData.filter(filterMovieRatings);
 
-// 1. Use the filter method to create a custom filtering function
-//  that returns the cities with a population increase greater than 15,000.
+  //  Check to make sure your are filtering your movies.
+  console.log(filteredMovies);
 
-function filterCities(city) {
-  return parseInt(city.Increase_from_2016) > 15000;
-}
+  // Use the map method with the arrow function to return all the filtered movie titles.
+  var titles = filteredMovies.map(movies =>  movies.title);
 
-// 2. Use filter() to pass the function as its argument
-var filteredCities = top15Cities.filter(filterCities);
+  // Use the map method with the arrow function to return all the filtered movie metascores.
+  var ratings = filteredMovies.map(movies => movies.metascore);
 
-//  Check to make sure your filtered your cities.
-console.log(filteredCities);
+  // Check your filtered metascores.
+  console.log(ratings);
 
-// 3. Use the map method with the arrow function to return all the filtered cities.
-var cities = filteredCities.map(city => city.City);
+  // Create your trace.
+  var trace = {
+    x: titles,
+    y: ratings,
+    type: "bar"
+  };
 
-//  Check your filtered cities
-console.log(cities);
+  // Create the data array for our plot
+  var data = [trace];
 
-// 4. Use the map method with the arrow function to return all the filtered cities population.
-var population = filteredCities.map(city => city.population);
+  // Define the plot layout
+  var layout = {
+    title: "The highest critically acclaimed movies.",
+    xaxis: { title: "Title" },
+    yaxis: { title: "Metascore (Critic) Rating"}
+  };
 
-//  Check the populations of your filtered cities
-console.log(population);
-
-// 5. Create your trace.
-var trace1 = {
-  x: cities,
-  y: population,
-  type: "bar"
-};
-
-// 6. Create the data array for our plot
-var data = [trace1];
-
-// 7. Define our plot layout
-var layout = {
-  title: "Cities that added more than 15,000 people in 2017",
-  xaxis: { title: "City" },
-  yaxis: { title: "2017 Population"}
-};
-
-// 8. Plot the chart to a div tag with id "bar-plot"
-Plotly.newPlot("bar-plot", data, layout);
-
-// Bonus challenge.
-//  Create a chart of the rate of growth (% of 2016 populations) of the filtered cities.
-//  Plot the chart to a div tag with id "rate-bar-plot"
-
-var pop_rate = filteredCities.map(city => {
-  // get the city's 2016 population 
-  var pop2016 = parseInt(city.population) - parseInt(city.Increase_from_2016);
-
-  var increase = parseInt(city.Increase_from_2016);
-
-  return 100 * increase / pop2016;
-})
-
-// Create the second trace.
-var trace2 = {
-  x: cities,
-  y: pop_rate,
-  type: "bar"
-};
-
-//  Create the data2 array for our plot
-var data2 = [trace2];
-
-// Define our plot layout
-var layout = {
-  title: "Cities that added more than 15,000 people in 2017",
-  xaxis: { title: "City" },
-  yaxis: { title: "Population Growth Rate (%)"}
-};
-
-// Plot the chart to a div tag with id "rate-bar-plot"
-Plotly.newPlot("rate-bar-plot", data2, layout);
+  // Plot the chart to a div tag with id "bar-plot"
+  Plotly.newPlot("bar-plot", data, layout);
+});
